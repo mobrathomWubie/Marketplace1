d99d696a7127d1741db9f5369b2f9d91c8148868
mebriewubie <mebriewubie@gmail.com>
2025-04-26 13:14:05 +0000

    Updated CSS and pages

--- client/src/components/layouts/AdminLayout.js
+++ client/src/components/layouts/AdminLayout.js
@@ -1,23 +1,28 @@
-import { Outlet, Link } from 'react-router-dom';
-import AdminNav from './AdminNav';
+import { Outlet, Link } from "react-router-dom";
+import Header from "../Header";
+import Footer from "../Footer";
 
 const AdminLayout = () => {
   return (
-    <div className="admin-layout">
-      <AdminNav />
-        <nav>
-            <ul>
-                <li><Link to="/admin/datasets">Admin Datasets</Link></li>
-                <li><Link to="/admin/datasets-list">Admin Datasets List</Link></li>
-                <li><Link to="/admin/upload">Upload Dataset</Link></li>
-                <li><Link to="/admin/edit-dataset">Edit Dataset</Link></li>
-                <li><Link to="/admin/delete-dataset">Delete Dataset</Link></li>
-            </ul>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow container mx-auto px-4">
+        <nav className="mb-4">
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/admin/datasets">Admin Datasets</Link>
+            </li>
+            <li>
+              <Link to="/admin/datasets-list">Admin Datasets List</Link>
+            </li>
+            <li><Link to="/admin/upload">Upload Dataset</Link></li>
+            <li><Link to="/admin/edit-dataset">Edit Dataset</Link></li>
+            <li><Link to="/admin/delete-dataset">Delete Dataset</Link></li>
+          </ul>
         </nav>
-      <main>
-        
-        <Outlet />
-      </main>
+        <main><Outlet /></main>
+      </div>
+      <Footer />
     </div>
   );
 };
